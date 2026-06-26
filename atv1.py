a53bf21449ca68ce5df879a92203f1cf991c41c7
Ataides Silva <ataides.silva@outlook.com>
2026-06-26 13:59:36 -0300

    Refactor age validation with user greeting
    Acrescentei umas linhas acima para praticar uma interação maior com o usuário.

--- atv1.py
+++ atv1.py
@@ -1,4 +1,14 @@
-idade = int (input("digite sua idade:"))
+#Mensagem inicial
+print('======Validador de Maioridade======')
+
+#Variavel nome usuario
+nome = str(input('Digite aqui seu nome :'))
+print('Olá {}, no campo abaixo, digite sua idade ok!'.format(nome))
+
+#Variavel - idade usuario
+idade = int(input('Insira sua idade :'))
+
+#Lógicas
 if idade >=18:
     print("você é maior de idade:")
 else:
